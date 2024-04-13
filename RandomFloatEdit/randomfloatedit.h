064615498a42d048d55dab5637ebf8c1feb62298
Alexandr Syrykh <alexandr.syryh@gmail.com>
2024-04-14 02:05:56 +0300

    Added a widget to search for rnd value in a range

---
+++ RandomFloatEdit/randomfloatedit.h
@@ -0,0 +1,32 @@
+#ifndef RANDOMFLOATEDIT_H
+#define RANDOMFLOATEDIT_H
+
+#include <QWidget>
+
+class FloatEdit;
+
+class RandomFloatEdit : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit RandomFloatEdit(QWidget *parent = 0);
+
+    void setRange(float min, float max);
+
+    void setValue(float value);
+    float value() const;
+
+signals:
+    void valueChanged(float value);
+
+private slots:
+    void locaValueChanged(float value);
+
+private:
+    QVector<FloatEdit*> m_edits;
+
+    float m_value;
+};
+
+
+#endif // RANDOMFLOATEDIT_H
