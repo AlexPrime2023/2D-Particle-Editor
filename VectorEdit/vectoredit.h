392aba9e456774e54bfda0a274bafec6dd1b9d56
Alexandr Syrykh <alexandr.syryh@gmail.com>
2024-04-14 00:25:20 +0300

    Added a widget for editing vectors

---
+++ VectorEdit/vectoredit.h
@@ -0,0 +1,26 @@
+#ifndef VECTOREDIT_H
+#define VECTOREDIT_H
+
+#include <QWidget>
+
+class FloatEdit;
+
+class VectorEdit : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit VectorEdit(int count, QWidget *parent = 0);
+    virtual ~VectorEdit();
+
+    void setMinimum(float value);
+    void setMaximum(float value);
+    void setIncrement(float value);
+protected:
+    int m_pCount;
+    QVector<FloatEdit*> edits;
+
+signals:
+    void valueChanged();
+};
+
+#endif // VECTOREDIT_H
