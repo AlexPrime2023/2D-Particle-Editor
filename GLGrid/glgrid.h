6912dbf23f1369b0370657b4bc1d2a53329ea0d1
Alexandr Syrykh <alexandr.syryh@gmail.com>
2024-04-13 19:35:54 +0300

    Added class GLGrid for alignment objects

---
+++ GLGrid/glgrid.h
@@ -0,0 +1,30 @@
+#ifndef GLGRID_H
+#define GLGRID_H
+
+#include <QOpenGLShaderProgram>
+#include <QOpenGLBuffer>
+
+#include "globject.h"
+
+class GLGrid : public GLObject
+{
+public:
+    GLGrid(float size, int segmentCount);
+    ~GLGrid();
+
+    void draw(const View &view) override;
+
+private:
+    void clear();
+    void initialize();
+
+    QOpenGLShaderProgram *m_shader = nullptr;
+
+    QOpenGLBuffer m_vertexBuffer;
+    QOpenGLBuffer m_indexBuffer;
+
+    QVector<QVector3D> m_vertex;
+    QVector<int> m_indices;
+};
+
+#endif // GLGRID_H
