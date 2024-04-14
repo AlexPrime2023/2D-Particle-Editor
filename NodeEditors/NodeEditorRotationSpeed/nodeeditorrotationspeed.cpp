4d21f9d4e42a178fb8e561607e5d01d3e46dce55
Alexandr Syrykh <alexandr.syryh@gmail.com>
2024-04-14 19:40:45 +0300

    Added base class for Editing Particle Rot Speed

---
+++ NodeEditors/NodeEditorRotationSpeed/nodeeditorrotationspeed.cpp
@@ -0,0 +1,38 @@
+#include "nodeeditorrotationspeed.h"
+
+#include "../FloatEdit/floatedit.h"
+#include "../RandomFloatEdit/randomfloatedit.h"
+
+NodeEditorRotationSpeed::NodeEditorRotationSpeed(QWidget *parent) :
+    NodeEditor(parent)
+{
+    m_rotationSpeed = new FloatEdit();
+    m_rotationSpeed->setRange(-50.0f, 50.0f);
+    addItem(QString("Particle Speed:"), m_rotationSpeed);
+
+    m_randomRotationSpeed = new RandomFloatEdit();
+    m_randomRotationSpeed->setRange(-50.0f, 50.0f);
+    addItem(QString("Particle Speed Random Range:"), m_randomRotationSpeed);
+
+    QObject::connect(m_rotationSpeed, &FloatEdit::valueChanged, this, &NodeEditorRotationSpeed::valueFloatEditChanged);
+    QObject::connect(m_randomRotationSpeed, &RandomFloatEdit::valueChanged, this, &NodeEditorRotationSpeed::valueFloatRandomEditChanged);
+}
+
+void NodeEditorRotationSpeed::resetEditor()
+{
+    m_rotationSpeed->setRange(0.0f, 5.0f);
+    m_rotationSpeed->setValue(0.0f);
+
+    m_randomRotationSpeed->setRange(0.0f, 5.0f);
+    m_randomRotationSpeed->setValue(0.0f);
+}
+
+void NodeEditorRotationSpeed::valueFloatEditChanged(float value)
+{
+    emit nodeEditorWidgetChanged(value);
+}
+
+void NodeEditorRotationSpeed::valueFloatRandomEditChanged(float value)
+{
+    m_rotationSpeed->setValue(value);
+}
---
+++ NodeEditors/NodeEditorRotationSpeed/nodeeditorrotationspeed.h
@@ -0,0 +1,25 @@
+#ifndef NODEEDITORROTATIONSPEED_H
+#define NODEEDITORROTATIONSPEED_H
+
+#include "nodeeditor.h"
+
+class FloatEdit;
+class RandomFloatEdit;
+
+class NodeEditorRotationSpeed : public NodeEditor
+{
+    Q_OBJECT
+public:
+    explicit NodeEditorRotationSpeed(QWidget *parent = nullptr);
+    void resetEditor() override;
+
+private:
+    FloatEdit *m_rotationSpeed;
+    RandomFloatEdit *m_randomRotationSpeed;
+
+private slots:
+    void valueFloatEditChanged(float value);
+    void valueFloatRandomEditChanged(float value);
+};
+
+#endif // NODEEDITORROTATIONSPEED_H
